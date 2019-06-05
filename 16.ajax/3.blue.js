cce6456b6a95586ba4db4819dc7248ac04d7c5e6
aroon <qyaroon@sina.com>
2019-06-05 22:13:42 +0800

    add ajax

---
+++ 16.ajax/3.blue.js
@@ -0,0 +1,10 @@
+var Promise = require('bluebird')
+var fs = Promise.promisifyAll(require('fs'))
+
+fs.readFileAsync('index.txt', 'utf8').then(function(contents) {
+    console.log(contents)
+}).catch(function(e) {
+    console.error(e.stack)
+})
+
+console.log('111')
